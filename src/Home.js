09e9b566a330f585d770a19c9cc2c783bce58480
Amjad0202 <mamamjadali@gmail.com>
2024-11-26 19:04:56 +0530

    Add Navbar and Home components; update App layout with dynamic content

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <home className="content">
+            <p>whatsup world</p>
+            <p>im having fun with coding</p>
+        </home>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+       <><nav className="content">
+            <h1>hi guys</h1>
+        </nav><div className="content">
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+            </div></>
+    );
+}
+ 
+export default Navbar;
